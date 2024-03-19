21fff6cfb83771fa8615d323023b5715d41afd6c
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-19 23:14:29 +0000

    feat: user home-manager unstable

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # Any other flake needed
