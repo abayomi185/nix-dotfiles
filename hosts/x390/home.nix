da57c9c5b930e551cd69ebef5b3ad544d90e31e9
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-19 22:43:48 +0000

    feat: add applications to x390

--- hosts/x390/home.nix
+++ hosts/x390/home.nix
@@ -54,7 +54,7 @@
 
   # Add stuff for your user as you see fit:
   programs.neovim.enable = true;
-  home.packages = with pkgs; [ spotify ];
+  home.packages = with pkgs; [ spotify wezterm ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
