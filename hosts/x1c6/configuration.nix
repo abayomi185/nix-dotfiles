db55ddf064eb74c6d05ceeb095a68d67452b8734
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-23 17:03:27 +0000

    feat(x1c6): disable gnome DE

--- hosts/x1c6/configuration.nix
+++ hosts/x1c6/configuration.nix
@@ -100,8 +100,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
