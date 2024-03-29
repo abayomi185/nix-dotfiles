314a2c5ba55a5ac32763c2fba779e22596cf96e2
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-29 16:18:33 +0000

    feat(module): update zsh module

--- modules/home-manager/shell/zsh.nix
+++ modules/home-manager/shell/zsh.nix
@@ -6,6 +6,19 @@
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+
+      shellAliases = {
+        la = "ls -la";
+        update = "sudo nixos-rebuild switch";
+      };
+
+      # zplug = {
+      #   enable = true;
+      #   plugins = [
+      #     { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+      #   ];
+      # };
     };
   };
+
 }
