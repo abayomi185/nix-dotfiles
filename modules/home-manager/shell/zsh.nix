2208c91417b017bd643ff3933cdc485977a7f4b7
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-29 18:05:13 +0000

    feat(home): update zsh module

--- modules/home-manager/shell/zsh.nix
+++ modules/home-manager/shell/zsh.nix
@@ -7,18 +7,38 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
 
+      history.size = 10000;
+
       shellAliases = {
         la = "ls -la";
         check = "nix flake check";
         update = "sudo nixos-rebuild switch";
       };
 
-      # zplug = {
-      #   enable = true;
-      #   plugins = [
-      #     { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      #   ];
-      # };
+      # zshrc equivalent
+      initExtra = ''
+      '';
+
+      # zshenv equivalent
+      envExtra = ''
+        # export PATH=$HOME/bin:$PATH
+        # Other environment variables or initialization commands
+      '';
+
+      # zprofile equivalent
+      profileExtra = ''
+      '';
+
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "plugins/git"; tags = [ "from:oh-my-zsh" ]; }
+          { name = "plugins/fzf"; tags = [ "from:oh-my-zsh" ]; }
+          { name = "plugins/ripgrep"; tags = [ "from:oh-my-zsh" ]; }
+          { name = "plugins/vi-mode"; tags = [ "from:oh-my-zsh" ]; }
+          # { name = "jeffreytse/zsh-vi-mode"; }
+        ];
+      };
     };
   };
 
