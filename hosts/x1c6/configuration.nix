6addb738085c365ae713edaf6474a2f052f024cf
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-29 18:04:46 +0000

    feat(x1c6): update i18n config

--- hosts/x1c6/configuration.nix
+++ hosts/x1c6/configuration.nix
@@ -20,6 +20,7 @@
       outputs.nixosModules.fonts
       # Apps: See ../../modules/nixos/apps/default.nix
       outputs.nixosModules.apps.neovim
+      outputs.nixosModules.apps.ripgrep
     ];
 
   nixpkgs = {
@@ -92,18 +93,18 @@
   time.timeZone = "Europe/London";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
   };
 
   # Enable the X11 windowing system.
