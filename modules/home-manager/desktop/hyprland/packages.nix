43232d8cd7e1b7ce9099433f92f183cc79235dd4
Yomi Ikuru <captyomjnr@gmail.com>
2024-03-31 16:17:04 +0100

    feat: add packages for hyprland

--- modules/home-manager/desktop/hyprland/packages.nix
+++ modules/home-manager/desktop/hyprland/packages.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     # prtsc # check https://github.com/spikespaz/dotfiles
+    iwgtk
+    libnotify
+    lxqt.pavucontrol-qt
+    wireplumber
   ];
 }
