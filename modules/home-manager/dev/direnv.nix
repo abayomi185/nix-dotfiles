474a2f558d4796b25cddf0d317fdf809c9e4aeb0
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-02 13:45:00 +0100

    feat: add nix-direnv

---
+++ modules/home-manager/dev/direnv.nix
@@ -0,0 +1,11 @@
+{
+  programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    zsh.enable = true; # see note on other shells below
+  };
+}
