ea448d03e05fce1269b8ccba384156f5b9e4a0d6
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-04 20:28:58 +0100

    feat(home): add global packages for neovim with nix and lua

--- modules/home-manager/apps/neovim.nix
+++ modules/home-manager/apps/neovim.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.neovim = {
     enable = true;
     extraPackages = with pkgs; [
+      # Global Lua packages
       lua-language-server
       stylua
       selene
+      # Global Nix packages
+      nil
+      alejandra
     ];
   };
 }
