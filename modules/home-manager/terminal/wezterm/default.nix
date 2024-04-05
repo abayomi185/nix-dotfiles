6106724a006d0794a4464ff2945e593cd5fa273b
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-05 16:21:37 +0100

    feat(home): use symlink for wezterm and not readfile

--- modules/home-manager/terminal/wezterm/default.nix
+++ modules/home-manager/terminal/wezterm/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [wezterm];
 
-  home.file.".config/wezterm/wezterm.lua".text = builtins.readFile ./wezterm.lua;
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 }
