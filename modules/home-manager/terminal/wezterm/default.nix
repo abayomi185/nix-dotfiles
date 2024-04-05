44adc29204df3dfe9c6113fe3cc8a699569e92ba
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-05 17:19:53 +0100

    feat(home): improve wezterm config

--- modules/home-manager/terminal/wezterm/default.nix
+++ modules/home-manager/terminal/wezterm/default.nix
@@ -1,5 +1,12 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [wezterm];
+{...}: {
+  # home.packages = with pkgs; [wezterm];
 
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    # extraConfig = builtins.readFile ./wezterm.lua; # symlink preferred
+  };
+
+  # Set up symlink to wezterm.lua
   xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 }
