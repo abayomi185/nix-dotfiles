e2b5e4a4313011a365675d60582720df52ca36e5
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-06 23:16:00 +0100

    feat(darwin): update yabai config

--- modules/darwin/desktop/yabai/default.nix
+++ modules/darwin/desktop/yabai/default.nix
@@ -2,5 +2,7 @@
   services.yabai = {
     enable = true;
     extraConfig = builtins.readFile ./yabairc;
+    # extraConfig = builtins.readFile "${./.}/yabairc";
+    # extraConfig = builtins.toFile "yabairc" (builtins.readFile ./yabairc);
   };
 }
