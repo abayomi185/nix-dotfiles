e5c9204b5c3e058a874eb31a39679ddedbc3cb89
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-06 19:01:28 +0100

    feat(home): add yabai and skhd apps

--- modules/home-manager/apps/default.nix
+++ modules/home-manager/apps/default.nix
@@ -3,5 +3,7 @@
   k9s = import ./k9s.nix;
   neovim = import ./neovim.nix;
   obsidian = import ./obsidian.nix;
+  skhd = import ./skhd.nix;
   tmux = import ./tmux.nix;
+  yabai = import ./yabai.nix;
 }
