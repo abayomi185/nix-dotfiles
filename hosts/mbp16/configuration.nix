2af7ec5eee7c59ec5e1bcfcd2563862adcf471de
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-06 01:29:56 +0100

    feat(mbp16): initial working config for darwin system

--- hosts/mbp16/configuration.nix
+++ hosts/mbp16/configuration.nix
@@ -1,8 +1,52 @@
-{ ... }: {
-  imports = [ ];
+{ config
+, pkgs
+, ...
+}:
+{
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-  environment.systemPackages = with pkgs; [ ];
+  nix.settings = {
+    # Enable flakes and new 'nix' command
+    experimental-features = "nix-command flakes";
+    # Deduplicate and optimize nix store
+    auto-optimise-store = true;
+  };
 
-  system = { };
+  # Define a user account
+  users.users = {
+    yomi = {
+      home = "/Users/yomi";
+      shell = "${pkgs.zsh}/bin/zsh";
+
+      openssh.authorizedKeys.keys = [
+        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      ];
+
+      packages = with pkgs; [
+        # NOTE: Packages are installed via home-manager
+        home-manager
+        # firefox
+        # thunderbird
+      ];
+    };
+  };
+
+  # Creates global /etc/zshrc that loads the nix-darwin environment
+  programs.zsh.enable = true; # Important!
+
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [ ];
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 }
