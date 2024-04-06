641d749a49ddec0dc5516bc55c910edfb445eecc
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-06 16:46:04 +0100

    feat(x1c6): update zsh config

--- hosts/x1c6/zsh.nix
+++ hosts/x1c6/zsh.nix
@@ -1,22 +1,7 @@
+# See common config here: ../../modules/home-manager/shell/zsh.nix
 {
   # Common configuration for Zsh
   programs.zsh = {
-    enable = true;
-
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    history.size = 10000;
-
-    shellAliases = {
-      la = "ls -la";
-      check = "nix flake check";
-      update = "sudo nixos-rebuild switch";
-      garbage = "sudo nix-collect-garbage --delete-older-than";
-      develop = "nix develop -c $SHELL";
-    };
-
     # zshenv equivalent
     envExtra = ''
       export lang=en_us.utf-8
@@ -24,28 +9,11 @@
       # other environment variables or initialization commands
     '';
 
-    zplug = {
-      enable = true;
-      plugins = [
-        {
-          name = "plugins/git";
-          tags = ["from:oh-my-zsh"];
-        }
-        {
-          name = "plugins/vi-mode";
-          tags = ["from:oh-my-zsh"];
-        }
-        {
-          name = "plugins/direnv";
-          tags = ["from:oh-my-zsh"];
-        }
-      ];
-    };
+    # zplug = {
+    #   plugins = [
+    #   ];
+    # };
   };
 
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
-    nix-direnv.enable = true;
-  };
+  programs.direnv.enable = true;
 }
