a701be1a22ea59c0968e3398e2d008ef96e11d70
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-09 23:51:03 +0100

    feat: enable discord app

--- hosts/x1c6/home.nix
+++ hosts/x1c6/home.nix
@@ -19,6 +19,7 @@
 
     # Apps - See ../../modules/home-manager/apps/default.nix
     outputs.homeManagerModules.apps.bat
+    outputs.homeManagerModules.apps.discord
     outputs.homeManagerModules.apps.k9s
     outputs.homeManagerModules.apps.neovim
     outputs.homeManagerModules.apps.obsidian
