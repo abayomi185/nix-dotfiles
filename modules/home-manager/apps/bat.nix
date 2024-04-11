11973555a4d1e27dad6770af26a5a70e7b463824
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-11 22:18:39 +0100

    sytle(home): run alejandra and remove bat theme

--- modules/home-manager/apps/bat.nix
+++ modules/home-manager/apps/bat.nix
@@ -1,12 +1,12 @@
-{ pkgs
-, lib
-, ...
+{
+  pkgs,
+  lib,
+  ...
 }: {
   # home.packages = with pkgs; [bat];
   # programs.zsh.envExtra = lib.mkBefore "export BAT_THEME=GitHub";
 
   programs.bat = {
     enable = true;
-    config.theme = "GitHub";
   };
 }
