9809bc38eb1623d6e6f25b67d74a35deea4af7d8
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-20 00:36:17 +0100

    feat(home): update rofi config

--- modules/home-manager/desktop/rofi/default.nix
+++ modules/home-manager/desktop/rofi/default.nix
@@ -2,93 +2,268 @@
   pkgs,
   config,
   ...
-}: {
-  # See this dotfile: https://github.com/karstenpedersen/dotfiles
+}: let
+  palette = config.colorScheme.palette;
+in {
   programs.rofi = {
     enable = true;
+
     package = pkgs.rofi-wayland;
-    pass = {
-      enable = true;
-      package = pkgs.rofi-pass-wayland;
-      extraConfig = ''
-        backend=wtype
-        clipboard_backend=wl-clipboard
-      '';
-    };
-    font = "Noto Sans 12";
+
+    plugins = with pkgs; [
+      rofi-calc
+      rofi-emoji
+    ];
+
     theme = let
       inherit (config.lib.formats.rasi) mkLiteral;
     in {
       "*" = {
-        background = mkLiteral "#${config.colorScheme.colors.base01}";
-        background-alt = mkLiteral "#${config.colorScheme.colors.base00}";
-        foreground = mkLiteral "#${config.colorScheme.colors.base04}";
-        foreground-alt = mkLiteral "#${config.colorScheme.colors.base05}";
-        primary = mkLiteral "#${config.colorScheme.colors.base06}aa";
-
-        background-color = mkLiteral "transparent";
-        text-color = mkLiteral "@foreground";
-        accent-color = mkLiteral "@foreground";
-
-        margin = 0;
-        padding = 0;
-        spacing = 0;
-        border = 0;
-
-        width = mkLiteral "800px";
+        bg = mkLiteral "#${palette.base00}";
+        background-color = mkLiteral "@bg";
       };
-      window = {
-        text-color = mkLiteral "@foreground";
-        background-color = mkLiteral "@background";
-        border = mkLiteral "2px";
-        border-color = mkLiteral "@primary";
-        border-radius = mkLiteral "3px";
-        padding = mkLiteral "6px";
+
+      "configuration" = {
+        modi = "run,drun";
+        show-icons = true;
+        icon-theme = "Papirus";
+        location = 0;
+        font = "JetBrains Nerd Font 12";
+        drun-display-format = "{icon} {name}";
+        display-drun = "   Apps ";
+        display-run = "   Run ";
+        display-filebrowser = "   File ";
+      };
+
+      "window" = {
+        width = mkLiteral "25%";
         transparency = "real";
+        orientation = mkLiteral "vertical";
+        border = mkLiteral "0 px";
+        border-color = mkLiteral "#${palette.base0F}";
+        border-radius = mkLiteral "10px";
       };
-      mainbox = {
-        children = mkLiteral "[inputbar, listview]";
+
+      "mainbox" = {
+        children = map mkLiteral ["inputbar" "listview" "mode-switcher"];
       };
-      inputbar = {
-        text-color = mkLiteral "@foreground";
-        background-color = mkLiteral "@background";
-        padding = mkLiteral "8px 8px";
-        spacing = mkLiteral "8px";
-        children = mkLiteral "[prompt, entry]";
+
+      # ELEMENT
+      # -----------------------------------
+
+      "element" = {
+        padding = mkLiteral "8 14";
+        text-color = mkLiteral "#${palette.base05}";
+        border-radius = mkLiteral "5px";
       };
-      prompt = {
-        text-color = mkLiteral "@foreground";
-      };
-      entry = {
-        text-color = mkLiteral "@foreground-alt";
-      };
-      listview = {
-        lines = 10;
-        scrollbar = true;
-      };
-      scrollbar = {
-        background-color = mkLiteral "@background-alt";
-        handle-color = mkLiteral "@foreground-alt";
-        margin-left = mkLiteral "6px";
-        handle-width = mkLiteral "5px";
-      };
-      element = {
-        text-color = mkLiteral "@foreground";
-        padding = mkLiteral "6px 8px";
-        spacing = mkLiteral "8px";
-        children = mkLiteral "[element-text]";
-      };
+
       "element selected" = {
-        text-color = mkLiteral "@foreground-alt";
-        background-color = mkLiteral "@background-alt";
+        text-color = mkLiteral "#${palette.base01}";
+        background-color = mkLiteral "#${palette.base0C}";
       };
-      element-text = {
+
+      "element-text" = {
+        background-color = mkLiteral "inherit";
         text-color = mkLiteral "inherit";
       };
+
+      "element-icon" = {
+        size = mkLiteral "24px";
+        background-color = mkLiteral "inherit";
+        padding = mkLiteral "0 6 0 0";
+        alignment = mkLiteral "vertical";
+      };
+
+      "listview" = {
+        columns = 1;
+        lines = 7;
+        padding = mkLiteral "8 0";
+        fixed-height = true;
+        fixed-columns = true;
+        fixed-lines = true;
+        border = mkLiteral "0 10 6 10";
+      };
+
+      # INPUT BAR
+      # ------------------------------------------------
+
+      "entry" = {
+        text-color = mkLiteral "#${palette.base05}";
+        padding = mkLiteral "10 10 0 0";
+        margin = mkLiteral "0 -2 0 0";
+      };
+
+      "inputbar" = {
+        padding = mkLiteral "0 0 0";
+        margin = mkLiteral "0 0 0 0";
+      };
+
+      "prompt" = {
+        text-color = mkLiteral "#${palette.base0D}";
+        padding = mkLiteral "10 6 0 10";
+        margin = mkLiteral "0 -2 0 0";
+      };
+
+      # Mode Switcher
+      # ------------------------------------------------
+
+      "mode-switcher" = {
+        border-color = mkLiteral "#${palette.base0F}";
+        spacing = 0;
+      };
+
+      "button" = {
+        padding = mkLiteral "10px";
+        background-color = mkLiteral "@bg";
+        text-color = mkLiteral "#${palette.base01}";
+        vertical-align = mkLiteral "0.5";
+        horizontal-align = mkLiteral "0.5";
+      };
+
+      "button selected" = {
+        background-color = mkLiteral "@bg";
+        text-color = mkLiteral "#${palette.base0F}";
+      };
+
+      "message" = {
+        background-color = mkLiteral "@bg";
+        margin = mkLiteral "2px";
+        padding = mkLiteral "2px";
+        border-radius = mkLiteral "5px";
+      };
+
+      "textbox" = {
+        padding = mkLiteral "6px";
+        margin = mkLiteral "20px 0px 0px 20px";
+        text-color = mkLiteral "#${palette.base0F}";
+        background-color = mkLiteral "@bg";
+      };
     };
-    extraConfig = {
-      # show-icons = true;
-      terminal = "wezterm";
-    };
+
+    # theme = ''
+    #   @theme "/dev/null"
+    #
+    #   * {
+    #     bg: #${palette.base00};
+    #     background-color: @bg;
+    #   }
+    #
+    #   configuration {
+    #     modi:		    "run,filebrowser,drun";
+    #     show-icons:	    true;
+    #     icon-theme:	    "Papirus";
+    #     location:		    0;
+    #     font:		    "JetBrains Nerd Font 16";
+    #     drun-display-format:  "{icon} {name}";
+    #     display-drun:	    "   Apps ";
+    #     display-run:	    "   Run ";
+    #     display-filebrowser:  "   File ";
+    #   }
+    #
+    #   window {
+    #     width: 45%;
+    #     transparency: "real";
+    #     orientation: vertical;
+    #     border: 2px ;
+    #     border-color: #${palette.base0F};
+    #     border-radius: 10px;
+    #   }
+    #
+    #   mainbox {
+    #     children: [ inputbar, listview, mode-switcher ];
+    #   }
+    #
+    #   // ELEMENT
+    #   // -----------------------------------
+    #
+    #   element {
+    #     padding: 8 14;
+    #     text-color: #${palette.base05};
+    #     border-radius: 5px;
+    #   }
+    #
+    #   element selected {
+    #     text-color: #${palette.base01};
+    #     background-color: #${palette.base0C};
+    #   }
+    #
+    #   element-text {
+    #     background-color: inherit;
+    #     text-color: inherit;
+    #   }
+    #
+    #   element-icon {
+    #     size: 24 px;
+    #     background-color: inherit;
+    #     padding: 0 6 0 0;
+    #     alignment: vertical;
+    #   }
+    #
+    #   listview {
+    #     columns: 2;
+    #     lines: 9;
+    #     padding: 8 0;
+    #     fixed-height: true;
+    #     fixed-columns: true;
+    #     fixed-lines: true;
+    #     border: 0 10 6 10;
+    #   }
+    #
+    #   // INPUT BAR
+    #   //------------------------------------------------
+    #
+    #   entry {
+    #     text-color: #${palette.base05};
+    #     padding: 10 10 0 0;
+    #     margin: 0 -2 0 0;
+    #   }
+    #
+    #   inputbar {
+    #     background-image: url("~/.config/rofi/rofi.jpg", width);
+    #     padding: 180 0 0;
+    #     margin: 0 0 0 0;
+    #   }
+    #
+    #   prompt {
+    #     text-color: #${palette.base0D};
+    #     padding: 10 6 0 10;
+    #     margin: 0 -2 0 0;
+    #   }
+    #
+    #   // Mode Switcher
+    #   //------------------------------------------------
+    #
+    #   mode-switcher {
+    #     border-color:   #${palette.base0F};
+    #     spacing:	      0;
+    #   }
+    #
+    #   button {
+    #     padding:	      10px;
+    #     background-color: @bg;
+    #     text-color:	      #${palette.base01};
+    #     vertical-align:   0.5;
+    #     horizontal-align: 0.5;
+    #   }
+    #
+    #   button selected {
+    #     background-color: @bg;
+    #     text-color: #${palette.base0F};
+    #   }
+    #
+    #   message {
+    #     background-color: @bg;
+    #     margin: 2px;
+    #     padding: 2px;
+    #     border-radius: 5px;
+    #   }
+    #
+    #   textbox {
+    #     padding: 6px;
+    #     margin: 20px 0px 0px 20px;
+    #     text-color: #${palette.base0F};
+    #     background-color: @bg;
+    #   }
+    # '';
   };
 }
