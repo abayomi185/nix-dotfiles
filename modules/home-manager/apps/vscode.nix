794e63896ec0a5b48edc64d86656c7f7a2acba4f
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-20 17:20:31 +0100

    feat(home): add vscode app

--- modules/home-manager/apps/vscode.nix
+++ modules/home-manager/apps/vscode.nix
@@ -1,3 +1,12 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [vscode];
+  # home.packages = with pkgs; [
+  #   vscode-fhs
+  # ];
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      ms-vscode-remote.remote-ssh
+      vscodevim.vim
+    ];
+  };
 }
