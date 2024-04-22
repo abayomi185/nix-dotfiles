2813f1f53c6b90baa29e90de2952e96ca920fa5c
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-22 18:02:43 +0100

    feat(home): add vscode extensions

--- modules/home-manager/apps/vscode.nix
+++ modules/home-manager/apps/vscode.nix
@@ -7,6 +7,16 @@
     extensions = with pkgs.vscode-extensions; [
       ms-vscode-remote.remote-ssh
       vscodevim.vim
+      github.copilot
+      github.copilot-chat
+      eamodio.gitlens
+      donjayamanne.githistory
+      emroussel.atomize-atom-one-dark-theme
+      vscode-icons-team.vscode-icons
+      oderwat.indent-rainbow
+      alefragnani.bookmarks
+      kamikillerto.vscode-colorize
+      esbenp.prettier-vscode
     ];
   };
 }
