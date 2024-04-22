3656cdb4436edadd614f7384ec3e84c4b18aae56
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-23 00:28:20 +0100

    chore(home): reorder hyprland packages

--- modules/home-manager/desktop/hyprland/packages.nix
+++ modules/home-manager/desktop/hyprland/packages.nix
@@ -1,11 +1,12 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     # prtsc # check https://github.com/spikespaz/dotfiles
-    networkmanagerapplet
-    libnotify
-    lxqt.pavucontrol-qt
-    wireplumber
+
     brightnessctl
     killall
+    libnotify
+    pavucontrol
+    networkmanagerapplet
+    wireplumber
   ];
 }
