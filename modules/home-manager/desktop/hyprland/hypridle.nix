da08becaf8aeb82a12350a78f0d6a333fe78458a
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-23 13:30:56 +0100

    feat(home): update hypridle config

--- modules/home-manager/desktop/hyprland/hypridle.nix
+++ modules/home-manager/desktop/hyprland/hypridle.nix
@@ -1,11 +1,11 @@
-{
+{pkgs, ...}: {
   services.hypridle = {
     enable = true;
 
-    lockCmd = "pidof hyprlock || hyprlock";
+    lockCmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
 
-    beforeSleepCmd = "loginctl lock-session";
-    afterSleepCmd = "hyprctl dispatch dpms on";
+    beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
+    afterSleepCmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
 
     listeners = [
       # {
@@ -15,21 +15,21 @@
       # }
       {
         timeout = 405; # 6 minutes 50 seconds
-        onTimeout = "brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
-        onResume = "brightnessctl -r"; # monitor backlight restore.
+        onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
+        onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r"; # monitor backlight restore.
       }
       {
         timeout = 420; # 7 minutes
-        onTimeout = "hyprctl dispatch dpms off"; # screen off when timeout has passed
-        onResume = "hyprctl dispatch dpms on"; # screen on when activity is detected
+        onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off"; # screen off when timeout has passed
+        onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on"; # screen on when activity is detected
       }
       {
         timeout = 430; # 7 minutes 10 seconds
-        onTimeout = "hyprlock"; # screen off when timeout has passed
+        onTimeout = ''${pkgs.hyprlock}/bin/hyprlock''; # screen off when timeout has passed
       }
       {
         timeout = 600; # 10 minutes
-        onTimeout = "systemctl suspend"; # screen off when timeout has passed
+        onTimeout = "${pkgs.systemd}/bin/systemctl suspend"; # screen off when timeout has passed
       }
     ];
   };
