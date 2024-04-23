4e87031421536cacf4c4c5aa73c68cc1e810f9cf
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-23 20:45:02 +0100

    feat(home): update xdg-portal module

--- modules/home-manager/desktop/xdg-desktop-portal.nix
+++ modules/home-manager/desktop/xdg-desktop-portal.nix
@@ -1,10 +1,16 @@
 {pkgs, ...}: {
   xdg.portal = {
     enable = true;
+    xdgOpenUsePortal = true;
+
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
+
     extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-kde
       xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
     ];
   };
 }
