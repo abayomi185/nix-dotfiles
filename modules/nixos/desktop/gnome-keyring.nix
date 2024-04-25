e32f9534e0351c0d6bd35d7cb16a94ba25cb193c
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-25 22:13:03 +0100

    feat(nixos): remove gnome-keyring module

--- modules/nixos/desktop/gnome-keyring.nix
+++
@@ -1,9 +0,0 @@
-# GNOME Keyring for storing/encrypting sycrets
-# apps like vscode stores encrypted data using it
-{pkgs, ...}: {
-  services.gnome.gnome-keyring.enable = true;
-  environment.systemPackages = [pkgs.libsecret]; # libsecret API
-  security.pam.services.gdm.enableGnomeKeyring = true; # load gnome-keyring at startup
-  environment.variables.XDG_RUNTIME_DIR = "/run/user/$UID"; # set the runtime directory
-  programs.seahorse.enable = true; # enable the graphical frontend for managing
-}
