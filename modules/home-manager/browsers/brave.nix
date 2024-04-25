840ca7650636e17895f6d024c4b29649b89942db
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-25 22:08:19 +0100

    feat(home): use home manager config for brave browser

--- modules/home-manager/browsers/brave.nix
+++ modules/home-manager/browsers/brave.nix
@@ -3,13 +3,31 @@
 # If it doesn't, you will need to find a different approach to customize the package,
 # such as using wrapProgram in a pkgs.stdenv.mkDerivation
 {pkgs, ...}: {
-  nixpkgs.overlays = [
-    (self: super: {
-      brave = super.brave.override {
-        commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation";
-      };
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     brave = super.brave.override {
+  #       commandLineArgs = "--enable-features=TouchpadOverscrollHistoryNavigation";
+  #     };
+  #   })
+  # ];
+  #
+  # home.packages = with pkgs; [brave];
 
-  home.packages = with pkgs; [brave];
+  programs.brave = {
+    enable = true;
+    commandLineArgs = [
+      "--enable-features=TouchpadOverscrollHistoryNavigation"
+      # "--password-store=gnome-libsecret"
+    ];
+    extensions = [
+      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # Ublock Origin
+      {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # Dark Reader
+      {id = "hfjbmagddngcpeloejdejnfgbamkjaeg";} # Vimium C
+      {id = "nffaoalbilbmmfgbnbgppjihopabppdk";} # Video speed controller
+      {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # SponsorBlock
+      {id = "nngceckbapebfimnlniiiahkandclblb";} # Bitwarden
+      {id = "jhnleheckmknfcgijgkadoemagpecfol";} # Auto Tab Discard
+      {id = "niloccemoadcdkdjlinkgdfekeahmflj";} # Pocket
+    ];
+  };
 }
