a7739e28b0835a24bc78a9b7b0837c171e009eae
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-26 20:23:50 +0100

    feat(x1c6): enable deep sleep as default

--- hosts/x1c6/hardware-configuration.nix
+++ hosts/x1c6/hardware-configuration.nix
@@ -14,6 +14,7 @@
 
   boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
+  boot.kernelParams = ["mem_sleep_default=deep"];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
