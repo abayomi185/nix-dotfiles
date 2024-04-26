9f10f8541352ce884da17083c2385440b7e6c83b
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-26 20:25:24 +0100

    feat(home): add home manager pointerCursor with hyprcursor

--- modules/home-manager/desktop/hyprland/hyprcursor.nix
+++ modules/home-manager/desktop/hyprland/hyprcursor.nix
@@ -1,3 +1,9 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [hyprcursor];
+  home.pointerCursor = {
+    name = "phinger-cursors-dark";
+    package = pkgs.phinger-cursors;
+    size = 32;
+    gtk.enable = true;
+  };
 }
