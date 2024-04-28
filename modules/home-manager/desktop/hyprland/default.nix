b31bf2a133058c1005344703781c264d94f7eb2d
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-28 12:35:29 +0100

    feat(home): improve gesture behaviour in hyprland

--- modules/home-manager/desktop/hyprland/default.nix
+++ modules/home-manager/desktop/hyprland/default.nix
@@ -64,9 +64,10 @@
 
       gestures = {
         workspace_swipe = true;
-        workspace_swipe_forever = true;
+        workspace_swipe_forever = false;
         workspace_swipe_invert = true;
         workspace_swipe_fingers = 3;
+        workspace_swipe_cancel_ratio = 0.1;
       };
 
       xwayland = {
