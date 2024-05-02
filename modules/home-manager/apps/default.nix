317be8e625992fc2422950638b1127e97bddec0d
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-02 20:58:36 +0100

    feat(home): add prusa-slicer app

--- modules/home-manager/apps/default.nix
+++ modules/home-manager/apps/default.nix
@@ -10,7 +10,8 @@
   obs = import ./obs.nix;
   obsidian = import ./obsidian.nix;
   skhd = import ./skhd.nix;
-  orcaslicer = import ./orcaslicer.nix;
+  orca-slicer = import ./orca-slicer.nix;
+  prusa-slicer = import ./prusa-slicer.nix;
   spacedrive = import ./spacedrive.nix;
   tmux = import ./tmux.nix;
   tree = import ./tree.nix;
