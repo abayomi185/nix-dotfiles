b4b48f82fe236a047d611b39a0a861e714afa3e4
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-09 01:19:13 +0100

    feat(home): update hypridle service for hyprland - service was merged into home-manager

--- modules/home-manager/desktop/hyprland/hypridle.nix
+++ modules/home-manager/desktop/hyprland/hypridle.nix
@@ -2,35 +2,38 @@
   services.hypridle = {
     enable = true;
 
-    lockCmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+    settings = {
+      general = {
+        lockCmd = "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        afterSleepCmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+      };
 
-    beforeSleepCmd = "${pkgs.systemd}/bin/loginctl lock-session";
-    afterSleepCmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
-
-    listeners = [
-      # {
-      #   timeout = 500; # 5 minutes
-      #   # onTimeout = "";
-      #   # onResume = "";
-      # }
-      {
-        timeout = 405; # 6 minutes 50 seconds
-        onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
-        onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r"; # monitor backlight restore.
-      }
-      {
-        timeout = 420; # 7 minutes
-        onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off"; # screen off when timeout has passed
-        onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on"; # screen on when activity is detected
-      }
-      {
-        timeout = 430; # 7 minutes 10 seconds
-        onTimeout = ''${pkgs.hyprlock}/bin/hyprlock''; # screen off when timeout has passed
-      }
-      {
-        timeout = 600; # 10 minutes
-        onTimeout = "${pkgs.systemd}/bin/systemctl suspend"; # screen off when timeout has passed
-      }
-    ];
+      listener = [
+        # {
+        #   timeout = 500; # 5 minutes
+        #   # onTimeout = "";
+        #   # onResume = "";
+        # }
+        {
+          timeout = 405; # 6 minutes 50 seconds
+          onTimeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
+          onResume = "${pkgs.brightnessctl}/bin/brightnessctl -r"; # monitor backlight restore.
+        }
+        {
+          timeout = 420; # 7 minutes
+          onTimeout = "${pkgs.hyprland}/bin/hyprctl dispatch dpms off"; # screen off when timeout has passed
+          onResume = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on"; # screen on when activity is detected
+        }
+        {
+          timeout = 430; # 7 minutes 10 seconds
+          onTimeout = ''${pkgs.hyprlock}/bin/hyprlock''; # screen off when timeout has passed
+        }
+        {
+          timeout = 600; # 10 minutes
+          onTimeout = "${pkgs.systemd}/bin/systemctl suspend"; # screen off when timeout has passed
+        }
+      ];
+    };
   };
 }
