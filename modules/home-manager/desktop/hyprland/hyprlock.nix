31fcae4480f05b9342e6eda7cc005da36e0a1f1a
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-11 19:33:07 +0100

    fix(home): hyprlock config

--- modules/home-manager/desktop/hyprland/hyprlock.nix
+++ modules/home-manager/desktop/hyprland/hyprlock.nix
@@ -3,7 +3,9 @@
     enable = true;
 
     settings = {
-      # general = {};
+      general = {
+        hide_cursor = true;
+      };
 
       # images = [
       #   {}
@@ -17,10 +19,7 @@
           #color = rgba(255, 255, 255, 0.6)
           font_size = 120;
           font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
-          position = {
-            x = 0;
-            y = -300;
-          };
+          position = "0, -300";
           halign = "center";
           valign = "top";
         }
@@ -50,27 +49,21 @@
 
       input-field = [
         {
-          size = {
-            width = 250;
-            height = 60;
-          };
+          size = "250, 60";
+          position = "0, -80";
+          halign = "center";
+          valign = "center";
+          # font_family = "JetBrains Mono Nerd Font Mono";
+          placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
+          outer_color = "rgba(0, 0, 0, 0)";
+          inner_color = "rgba(0, 0, 0, 0.5)";
+          font_color = "rgb(200, 200, 200)";
           outline_thickness = 2;
           dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
           dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
           dots_center = true;
-          outer_color = "rgba(0, 0, 0, 0)";
-          inner_color = "rgba(0, 0, 0, 0.5)";
-          font_color = "rgb(200, 200, 200)";
           fade_on_empty = false;
-          # font_family = "JetBrains Mono Nerd Font Mono";
-          placeholder_text = ''<i><span foreground="##cdd6f4">Input Password...</span></i>'';
           hide_input = false;
-          position = {
-            x = 0;
-            y = -80;
-          };
-          halign = "center";
-          valign = "center";
         }
       ];
     };
