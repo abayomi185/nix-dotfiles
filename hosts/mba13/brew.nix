3c44829fba586311cf54207ee2be838b64ce53fa
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-17 00:50:11 +0100

    feat(mba13): add homebrew config

--- hosts/mba13/brew.nix
+++ hosts/mba13/brew.nix
@@ -1,6 +1,29 @@
 {
-  homebrew.casks = [
-    "discord"
-    "visual-studio-code"
-  ];
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      cleanup = "uninstall";
+    };
+
+    # global = {};
+
+    taps = [];
+
+    brews = [];
+
+    casks = [
+      "affinity-designer"
+      "affinity-photo"
+      "affinity-publisher"
+      "brave-browser"
+      "discord"
+      "obsidian"
+    ];
+
+    masApps = {
+      # Xcode = 497799835;
+      Wipr = 1320666476;
+    };
+  };
 }
