3d1f9ac83731cbc481915e38eeebd929625a2041
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-17 19:37:10 +0100

    fix(darwin): remove symlink from darwin karabiner elements module

--- modules/darwin/apps/karabiner-elements/default.nix
+++ modules/darwin/apps/karabiner-elements/default.nix
@@ -1,9 +1,5 @@
-{config, ...}: {
+{
   services.karabiner-elements = {
     enable = true;
   };
-
-  # Set up symlink to config file
-  xdg.configFile."karabiner/karabiner.json".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/darwin/apps/karabiner/karabiner.json";
 }
