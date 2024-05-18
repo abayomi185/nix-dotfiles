cf1746064010d81f9799fd61c61022d75c0df2ec
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-18 12:39:44 +0100

    feat(home): add xcodes cli app

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -3,5 +3,6 @@
   nodejs = import ./nodejs.nix;
   python = import ./python.nix;
   rust = import ./rust.nix;
+  xcodes = import ./xcodes.nix; # For managing Xcode versions
   zig = import ./zig.nix;
 }
