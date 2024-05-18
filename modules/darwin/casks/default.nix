0c58c7e03d8f1e6c9a373aecf0411cece590640f
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-18 10:28:44 +0100

    feat(darwin): add orca-slicer cask

--- modules/darwin/casks/default.nix
+++ modules/darwin/casks/default.nix
@@ -1,6 +1,7 @@
 {
   brave = import ./brave.nix;
   obsidian = import ./obsidian.nix;
+  orca-slicer = import ./orca-slicer.nix;
   raycast = import ./raycast.nix;
   serif-apps = import ./serif-apps.nix;
 }
