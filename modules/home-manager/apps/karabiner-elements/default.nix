6ce8843a8a1da23be86619cf0f4ead502434e5a7
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-19 22:59:52 +0100

    feat(home): update karabiner config and symlink

--- modules/home-manager/apps/karabiner-elements/default.nix
+++ modules/home-manager/apps/karabiner-elements/default.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
   # Set up symlink to karabiner config file
   xdg.configFile."karabiner/karabiner.json".source =
-    config.lib.file.mkOutOfStoreSymlink "/Users/yomi/nix-dotfiles/modules/darwin/apps/karabiner-elements/karabiner.json";
+    config.lib.file.mkOutOfStoreSymlink "/Users/yomi/nix-dotfiles/modules/home-manager/apps/karabiner-elements/karabiner.json";
 }
