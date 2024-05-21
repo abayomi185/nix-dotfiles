dec4484825ae2466f5c6475d704fe508aa9a020c
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-21 22:00:09 +0100

    feat(darwin): casks: add obs

--- modules/darwin/casks/default.nix
+++ modules/darwin/casks/default.nix
@@ -2,6 +2,7 @@
   autodesk-fusion = import ./autodesk-fusion.nix;
   brave = import ./brave.nix;
   notion = import ./notion.nix;
+  obs = import ./obs.nix;
   obsidian = import ./obsidian.nix;
   orca-slicer = import ./orca-slicer.nix;
   raycast = import ./raycast.nix;
