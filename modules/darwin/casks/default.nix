9602b4d9d7ea714e1bc58c0b65edb891509be763
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-23 21:45:33 +0100

    feat(mbp16): casks: comment out ocenaudio

--- modules/darwin/casks/default.nix
+++ modules/darwin/casks/default.nix
@@ -5,7 +5,7 @@
   notion = import ./notion.nix;
   obs = import ./obs.nix;
   obsidian = import ./obsidian.nix;
-  ocenaudio = import ./ocenaudio.nix;
+  # ocenaudio = import ./ocenaudio.nix;
   orca-slicer = import ./orca-slicer.nix;
   raycast = import ./raycast.nix;
   serif-apps = import ./serif-apps.nix;
