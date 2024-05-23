6022b2f638295eebfbd748b35f8141dc37699702
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-23 08:17:42 +0100

    feat(darwin): mas: add mas modules

---
+++ modules/darwin/mas/core.nix
@@ -0,0 +1,11 @@
+_:
+let
+  coreApps = {
+    DaisyDisk = 411643860;
+    HomeAssistant = 1099568401;
+    Wipr = 1320666476;
+  };
+in
+{
+  homebrew.masApps = coreApps;
+}
---
+++ modules/darwin/mas/productivity.nix
@@ -0,0 +1,12 @@
+_:
+let
+  productivityApps = {
+    ApplePages = 409201541;
+    AppleNumbers = 409203825;
+    AppleKeynote = 409183694;
+    # XnConvert = 436203431;
+  };
+in
+{
+  homebrew.masApps = productivityApps;
+}
