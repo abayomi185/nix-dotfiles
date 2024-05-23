5b5468836806bf728b488e9e373ffc1195c716b9
Yomi Ikuru <captyomjnr@gmail.com>
2024-05-23 21:37:06 +0100

    style(darwin): mas: run formatter for mas configs

--- modules/darwin/mas/productivity.nix
+++ modules/darwin/mas/productivity.nix
@@ -1,12 +1,10 @@
-_:
-let
+_: let
   productivityApps = {
     ApplePages = 409201541;
     AppleNumbers = 409203825;
     AppleKeynote = 409183694;
     # XnConvert = 436203431;
   };
-in
-{
+in {
   homebrew.masApps = productivityApps;
 }
