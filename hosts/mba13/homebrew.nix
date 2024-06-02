bf06344d78c8d1ea7bc6afc27b98db62bb81adc7
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-02 11:05:48 +0100

    feat(mba13): brews: enable aws dev tools

--- hosts/mba13/homebrew.nix
+++ hosts/mba13/homebrew.nix
@@ -5,6 +5,7 @@
 
     # Brews - See ../../modules/darwin/brews/default.nix
     outputs.darwinModules.brews.group_mobileDev
+    outputs.darwinModules.brews.group_awsDev
     outputs.darwinModules.brews.docker
     outputs.darwinModules.brews.sdl2
 
