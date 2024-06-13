35fea6a12787cd15472f0b1d12849b0ef283ccdb
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-13 23:42:33 +0100

    feat(mba13): brews: enable azure dev tools

--- hosts/mba13/homebrew.nix
+++ hosts/mba13/homebrew.nix
@@ -6,6 +6,7 @@
     # Brews - See ../../modules/darwin/brews/default.nix
     outputs.darwinModules.brews.group_mobileDev
     outputs.darwinModules.brews.group_awsDev
+    outputs.darwinModules.brews.group_azureDev
     outputs.darwinModules.brews.docker
     outputs.darwinModules.brews.sdl2
 
