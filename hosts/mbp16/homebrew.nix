d9b25be22565b1862803f576ace18cb22da966e4
Yomi Ikuru <captyomjnr@gmail.com>
2024-06-22 12:18:45 +0100

    feat(mbp16): enable multiple casks

--- hosts/mbp16/homebrew.nix
+++ hosts/mbp16/homebrew.nix
@@ -6,7 +6,16 @@
     # Brews - See ../../modules/darwin/brews/default.nix
 
     # Casks - See ../../modules/darwin/casks/default.nix
-    outputs.darwinModules.casks.ocenaudio
+    outputs.darwinModules.casks.brave
+    outputs.darwinModules.casks.calibre
+    outputs.darwinModules.casks.coconutbattery
+    outputs.darwinModules.casks.figma
+    outputs.darwinModules.casks.kicad
+    outputs.darwinModules.casks.obs
+    outputs.darwinModules.casks.obsidian
+    outputs.darwinModules.casks.orca-slicer
+    outputs.darwinModules.casks.raycast
+    outputs.darwinModules.casks.sublime-text
 
     # Mas - See ../../modules/darwin/mas/default.nix
     outputs.darwinModules.mas.coreApps
