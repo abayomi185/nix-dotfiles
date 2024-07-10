1a7a0df051c6dee412f6f8764e6247680bee1061
Yomi Ikuru <captyomjnr@gmail.com>
2024-07-10 22:09:08 +0100

    feat(vps): add configuration.nix

---
+++ hosts/vps/configuration.nix
@@ -0,0 +1,104 @@
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./networking.nix
+    ./wireguard.nix
+
+    # Containers
+    ../../containers/uptime-kuma/docker-compose.nix
+  ];
+
+  # Secrets
+  sops = {
+    age.sshKeyPaths = ["/home/cloud/.ssh/id_ed25519"];
+    defaultSopsFile = ./secrets.enc.yaml;
+  };
+
+  sops.secrets = {
+    "user/initial_password" = {
+      neededForUsers = true;
+    };
+    "ssh/authorized_keys/a" = {};
+    "ssh/authorized_keys/b" = {};
+    "ssh/authorized_keys/c" = {};
+  };
+
+  # Hetzner
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+  networking.hostName = "vps-arm64";
+  networking.domain = "";
+
+  programs.nix-ld.enable = true; # Enable nix-ld - it just works
+
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+
+  # Shell
+  programs.zsh = {enable = true;};
+  # Define a user account
+  users.users = {
+    cloud = {
+      isNormalUser = true;
+      description = "cloud";
+      shell = pkgs.zsh;
+      extraGroups = ["wheel" "docker"];
+      initialPassword = config.sops.secrets."user/initial_password".path;
+    };
+  };
+
+  # Containers
+  # virtualisation.docker.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings = {
+      # Required for container networking to be able to use names.
+      dns_enabled = true;
+    };
+  };
+  virtualisation.oci-containers.backend = "podman";
+
+  # Programs
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Services
+  services.openssh = {
+    enable = true;
+    # Let's not repeat last time's mistake
+    # settings = {
+    #   PermitRootLogin = "no";
+    # };
+  };
+  users.users.root.openssh.authorizedKeys.keys = [];
+  users.users.cloud.openssh.authorizedKeys.keys = [
+    config.sops.secrets."ssh/authorized_keys/a".path
+    config.sops.secrets."ssh/authorized_keys/b".path
+    config.sops.secrets."ssh/authorized_keys/c".path
+  ];
+
+  # Fail2ban
+  # services.fail2ban.enable = true;
+  # NixOS by default is pre-configured with SSH jail
+
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    zig
+    podman-tui
+    podman-compose
+    inputs.compose2nix.packages.aarch64-linux.default
+  ];
+
+  system.stateVersion = "23.11";
+}
