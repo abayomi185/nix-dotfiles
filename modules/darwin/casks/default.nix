83c466651a0912c6a2016397e5eb799bcf0a2beb
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-15 21:42:44 +0100

    feat(darwin): casks: add casks - firefox, ollama and bruno

--- modules/darwin/casks/default.nix
+++ modules/darwin/casks/default.nix
@@ -1,14 +1,17 @@
 {
   autodesk-fusion = import ./autodesk-fusion.nix;
   brave = import ./brave.nix;
+  bruno = import ./bruno.nix;
   calibre = import ./calibre.nix;
   coconutbattery = import ./coconutbattery.nix;
   figma = import ./figma.nix;
+  firefox = import ./firefox.nix;
   iina = import ./iina.nix;
   kicad = import ./kicad.nix;
   notion = import ./notion.nix;
   obs = import ./obs.nix;
   obsidian = import ./obsidian.nix;
+  ollama = import ./ollama.nix;
   # ocenaudio = import ./ocenaudio.nix;
   orbstack = import ./orbstack.nix;
   orca-slicer = import ./orca-slicer.nix;
