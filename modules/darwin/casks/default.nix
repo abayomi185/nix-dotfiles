b2311b48aa2d41ed85a686ba7587b765c97b059c
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-15 21:55:03 +0100

    feat(darwin): casks: add casks - devtoys, openscad and freecad

--- modules/darwin/casks/default.nix
+++ modules/darwin/casks/default.nix
@@ -4,14 +4,17 @@
   bruno = import ./bruno.nix;
   calibre = import ./calibre.nix;
   coconutbattery = import ./coconutbattery.nix;
+  devtoys = import ./devtoys.nix;
   figma = import ./figma.nix;
   firefox = import ./firefox.nix;
+  freecad = import ./freecad.nix;
   iina = import ./iina.nix;
   kicad = import ./kicad.nix;
   notion = import ./notion.nix;
   obs = import ./obs.nix;
   obsidian = import ./obsidian.nix;
   ollama = import ./ollama.nix;
+  openscad = import ./openscad.nix;
   # ocenaudio = import ./ocenaudio.nix;
   orbstack = import ./orbstack.nix;
   orca-slicer = import ./orca-slicer.nix;
