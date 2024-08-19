a259df977d2a0e56741a3b46ab715224aca5f28d
Yomi Ikuru <captyomjnr@gmail.com>
2024-08-19 17:43:30 +0100

    feat(home): neovim: use neovim from nixpkgs unstable

---
+++ modules/home-manager/apps/neovim-unstable.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  programs.neovim = {
+    enable = true;
+    package = pkgs.unstable.neovim-unwrapped;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      # Global Lua packages
+      lua-language-server
+      stylua
+      selene
+      # Global Nix packages
+      nil
+      alejandra
+      # Other Global
+      prettierd
+      taplo
+    ];
+  };
+}
