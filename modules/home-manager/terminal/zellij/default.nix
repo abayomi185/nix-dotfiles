c7afe2a283dab7b1c18821bcdf3c5cb5d30d003a
Yomi Ikuru <captyomjnr@gmail.com>
2024-10-23 00:57:50 +0100

    feat(home): terminal: zellij: add layouts

--- modules/home-manager/terminal/zellij/default.nix
+++ modules/home-manager/terminal/zellij/default.nix
@@ -6,4 +6,24 @@
   # Set up symlink to config file
   xdg.configFile."zellij/config.kdl".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/config.kdl";
+
+  xdg.configFile."zellij/layouts/primary.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/main-layout.kdl";
+  xdg.configFile."zellij/layouts/primary.swap.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/layout.swap.kdl";
+
+  xdg.configFile."zellij/layouts/secondary.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/secondary-layout.kdl";
+  xdg.configFile."zellij/layouts/secondary.swap.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/base-layout.swap.kdl";
+
+  xdg.configFile."zellij/layouts/tertiary.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/tertiary-layout.kdl";
+  xdg.configFile."zellij/layouts/tertiary.swap.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/base-layout.swap.kdl";
+
+  xdg.configFile."zellij/layouts/quaternary.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/quaternary-layout.kdl";
+  xdg.configFile."zellij/layouts/quaternary.swap.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-dotfiles/modules/home-manager/terminal/zellij/base-layout.swap.kdl";
 }
