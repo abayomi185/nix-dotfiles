85e201a097ed39b02f817c4587cbdc34b1fcbc3d
Yomi Ikuru <captyomjnr@gmail.com>
2024-11-04 20:44:40 +0000

    test(knode): update config for build check

--- hosts/knode/default.nix
+++ hosts/knode/default.nix
@@ -1,31 +1,18 @@
 {
   inputs,
-  pNodeId,
-  pK3sRole,
-  pK3sServerId,
-  pK3sClusterInit,
   outputs,
-  ...
-}: {
-  mkHost = {
-    host,
-    hardware,
-    stateVersion,
-    system,
-    timezone,
-    location,
-    extraOverlays,
-    extraModules,
-  }:
-    inputs.nixpkgs-stable.lib.nixosSystem
-    {
-      system = "x86_64-linux";
-      specialArgs = {
-        inherit inputs outputs pNodeId pK3sRole pK3sServerId pK3sClusterInit;
-      };
-      modules = [
-        ./configuration.nix
-        inputs.sops-nix.nixosModules.sops
-      ];
-    };
+  pNodeId,
+  pK3sRole ? "agent",
+  pK3sClusterInit ? false,
+  pK3sServerId ? "1",
+}:
+inputs.nixpkgs-stable.lib.nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = {
+    inherit inputs outputs pNodeId pK3sRole pK3sServerId pK3sClusterInit;
+  };
+  modules = [
+    ./configuration.nix
+    inputs.sops-nix.nixosModules.sops
+  ];
 }
