a2ed15ee9611c51583e925d101ebb8fcf402cf43
Yomi Ikuru <captyomjnr@gmail.com>
2024-11-07 00:47:31 +0000

    feat(knode): add minimal config for getting things setup

---
+++ hosts/knode/minimal-configuration.nix
@@ -0,0 +1,54 @@
+{modulesPath, ...}: let
+  timeZone = "Europe/London";
+  defaultLocale = "en_GB.UTF-8";
+in {
+  imports = [
+    # Include the default lxc/lxd configuration.
+    "${modulesPath}/virtualisation/lxc-container.nix"
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  boot.isContainer = true;
+  networking.hostName = "knode";
+
+  time.timeZone = timeZone;
+
+  i18n = {
+    defaultLocale = defaultLocale;
+    extraLocaleSettings = {
+      LC_ADDRESS = defaultLocale;
+      LC_IDENTIFICATION = defaultLocale;
+      LC_MEASUREMENT = defaultLocale;
+      LC_MONETARY = defaultLocale;
+      LC_NAME = defaultLocale;
+      LC_NUMERIC = defaultLocale;
+      LC_PAPER = defaultLocale;
+      LC_TELEPHONE = defaultLocale;
+      LC_TIME = defaultLocale;
+    };
+  };
+
+  # Supress systemd units that don't work because of LXC.
+  # https://blog.xirion.net/posts/nixos-proxmox-lxc/#configurationnix-tweak
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
+
+  networking.interfaces = {
+    eth0 = {
+      ipv4.addresses = [
+        {
+          address = "10.0.1.49";
+          prefixLength = 24;
+        }
+      ];
+    };
+  };
+  networking.defaultGateway = "10.0.1.1";
+  networking.nameservers = ["1.0.1.53"];
+
+  system.stateVersion = "24.05";
+}
