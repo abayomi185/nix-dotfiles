e3689e0cd0d2af6dcb74944c5b3c80b7244c19fa
Yomi Ikuru <captyomjnr@gmail.com>
2024-12-08 17:08:57 +0000

    feat: comment out wezterm-nightly

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,13 +3,13 @@
 {pkgs, ...}: {
   # example = pkgs.callPackage ./example { };
 
-  wezterm-nightly = let
-    callPackage =
-      if pkgs.stdenv.isDarwin
-      then pkgs.darwin.apple_sdk_11_0.callPackage
-      else pkgs.callPackage;
-  in
-    callPackage ./wezterm-nightly {};
+  # wezterm-nightly = let
+  #   callPackage =
+  #     if pkgs.stdenv.isDarwin
+  #     then pkgs.darwin.apple_sdk_11_0.callPackage
+  #     else pkgs.callPackage;
+  # in
+  #   callPackage ./wezterm-nightly {};
 
   firefly-iii-data-importer = pkgs.callPackage ./firefly-iii-data-importer {};
 }
