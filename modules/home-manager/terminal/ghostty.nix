a288cc23a98cc2a54856e51c6ad0af2ef767c3ce
Yomi Ikuru <captyomjnr@gmail.com>
2025-01-21 23:05:38 +0000

    feat(home/terminal): add ghostty terminal

---
+++ modules/home-manager/terminal/ghostty.nix
@@ -0,0 +1,10 @@
+{
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      theme = "dark:tokyonight,light:tokyonight-day";
+      macos-titlebar-style = "hidden";
+    };
+  };
+}
