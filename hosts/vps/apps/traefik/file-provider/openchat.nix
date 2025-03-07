6732aac571db0b015be590b0ea207957e307de6e
Yomi Ikuru <yomi+git_cloud_vps@yomitosh.com>
2025-03-07 15:32:15 +0000

    feat(vps): app/traefik: reduce confusion for open webui domain

--- hosts/vps/apps/traefik/file-provider/openchat.nix
+++ hosts/vps/apps/traefik/file-provider/openchat.nix
@@ -1,18 +1,18 @@
 {
   services.traefik.dynamicConfigOptions = {
     http.routers.openchat = {
-      rule = ''Host("openchat.{{env `MEDIA_DOMAIN_NAME`}}")'';
+      rule = ''Host("chat.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "openchat";
     };
     http.routers.openchat-secure = {
-      rule = ''Host("openchat.{{env `MEDIA_DOMAIN_NAME`}}")'';
+      rule = ''Host("chat.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "openchat";
       tls.certResolver = "letsencrypt";
       middlewares = ["global_rate_limit@file"];
     };
 
     http.services.openchat.loadBalancer.servers = [
-      {url = ''https://openchat.local.{{env `MEDIA_DOMAIN_NAME`}}'';}
+      {url = ''https://chat.local.{{env `MEDIA_DOMAIN_NAME`}}'';}
     ];
   };
 }
