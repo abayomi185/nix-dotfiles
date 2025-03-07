de15953187c3b46029f24cebcf4b3a28cd31086e
Yomi Ikuru <yomi+git_cloud_vps@yomitosh.com>
2025-03-07 15:45:18 +0000

    feat(vps): app/traefik: update chat endpoint to avoid needing additional certs

--- hosts/vps/apps/traefik/file-provider/chat.nix
+++ hosts/vps/apps/traefik/file-provider/chat.nix
@@ -1,18 +1,18 @@
 {
   services.traefik.dynamicConfigOptions = {
     http.routers.chat = {
-      rule = ''Host("chat.internal.{{env `MEDIA_DOMAIN_NAME`}}")'';
+      rule = ''Host("chat-internal.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "chat";
     };
     http.routers.chat-secure = {
-      rule = ''Host("chat.internal.{{env `MEDIA_DOMAIN_NAME`}}")'';
+      rule = ''Host("chat-internal.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "chat";
       tls.certResolver = "letsencrypt";
       middlewares = ["global_rate_limit@file" "authelia@file"];
     };
 
     http.services.chat.loadBalancer.servers = [
-      {url = ''https://chat.internal.local.{{env `MEDIA_DOMAIN_NAME`}}'';}
+      {url = ''https://chat-internal.local.{{env `MEDIA_DOMAIN_NAME`}}'';}
     ];
   };
 }
