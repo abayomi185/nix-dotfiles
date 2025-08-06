997395ab9aadb06c9f4cca3b720305fdaf6e5247
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-06 06:22:50 +0100

    feat(vps): set max grub configurations
    prevents boot partition from running out of space

--- hosts/vps/hardware-configuration.nix
+++ hosts/vps/hardware-configuration.nix
@@ -1,17 +1,18 @@
-{ modulesPath, ... }: {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
-  boot.kernelParams = [ "console=tty" ];
+{modulesPath, ...}: {
+  imports = [(modulesPath + "/profiles/qemu-guest.nix")];
+  boot.kernelParams = ["console=tty"];
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
     device = "nodev";
   };
+  boot.loader.grub.configurationLimit = 2;
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/6FB6-65E7";
     fsType = "vfat";
   };
-  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
-  boot.initrd.kernelModules = [ "nvme" ];
+  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
+  boot.initrd.kernelModules = ["nvme"];
   fileSystems."/" = {
     device = "/dev/sda1";
     fsType = "ext4";
