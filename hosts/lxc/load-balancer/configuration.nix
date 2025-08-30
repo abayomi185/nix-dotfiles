3ff8624ec3605e68d48727b252fa2ae7f1107fbb
Yomi Ikuru <captyomjnr@gmail.com>
2025-08-30 19:43:27 +0100

    feat(lxc/load-balancer): add alias to fetch, pull and rebuild config during test..trial and error

--- hosts/lxc/load-balancer/configuration.nix
+++ hosts/lxc/load-balancer/configuration.nix
@@ -95,6 +95,10 @@ in {
     };
   };
 
+  programs.bash.interactiveShellInit = ''
+    alias fetch_pull_rebuild="git fetch --all && git reset --hard origin/main && nixos-rebuild switch --flake .#load-balancer"
+  '';
+
   services.traefik = {
     enable = true;
     dynamicConfigOptions = traefik_dynamicConfig;
