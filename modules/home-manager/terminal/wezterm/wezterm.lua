f29541d0b07e36c8909ef3aa0507bd7b9542a6e0
Yomi Ikuru <captyomjnr@gmail.com>
2025-10-04 21:41:24 +0100

    feat(home/terminal/wezterm): add resurrect.wezterm plugin and add toggle_terminal.wez

--- modules/home-manager/terminal/wezterm/wezterm.lua
+++ modules/home-manager/terminal/wezterm/wezterm.lua
@@ -1,6 +1,11 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+-- Other Wezterm plugins
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+local toggle_terminal = wezterm.plugin.require("https://github.com/zsh-sage/toggle_terminal.wez")
+
 -- This table will hold the configuration.
 local config = {}
 
@@ -10,17 +15,10 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
 local color_scheme_light = "Solarized Light (Gogh)"
--- local color_scheme_light = "Batman"
 local color_scheme_dark = "Batman"
--- local color_scheme_dark = "Solarized Light (Gogh)"
--- local color_scheme = "Builtin Solarized Dark"
--- local color_scheme = "AdventureTime"
--- local color_scheme = "Andromeda"
 
 local function get_appearance()
 	if wezterm.gui then
@@ -38,8 +36,6 @@ end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 
--- config.font = wezterm.font('JetBrains Mono', { bold = false, italic = false })
-
 config.use_fancy_tab_bar = true
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
@@ -54,8 +50,6 @@ config.window_padding = {
 }
 config.adjust_window_size_when_changing_font_size = false
 
--- config.enable_wayland = false
--- config.front_end = "Software"
 config.front_end = "WebGpu"
 config.max_fps = 120
 
@@ -71,46 +65,128 @@ config.keys = {
 		mods = "SUPER",
 		action = wezterm.action.ActivateLastTab,
 	},
+
+	-- Resurrect
+	{
+		key = "w",
+		mods = "ALT",
+		action = wezterm.action_callback(function(win, pane)
+			-- Resurrect state is stored in resurrect plugin directory
+			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+			resurrect.window_state.save_window_action()
+		end),
+	},
+	{
+		key = "r",
+		mods = "ALT",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+
+				local opts = {
+					window = win:mux_window(),
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+					close_open_tabs = true,
+				}
+
+				if type == "workspace" then
+					local state = resurrect.state_manager.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.state_manager.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.state_manager.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end, {
+				ignore_tabs = true,
+				ignore_windows = true,
+				show_state_with_date = true,
+			})
+		end),
+	},
+	{
+		key = "r",
+		mods = "ALT|SHIFT",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
+				resurrect.delete_state(id)
+			end, {
+				title = "Delete State",
+				description = "Select session to delete and press Enter = accept, Esc = cancel, / = filter",
+				fuzzy_description = "Search session to delete: ",
+				is_fuzzy = true,
+				ignore_tabs = true,
+				ignore_windows = true,
+			})
+		end),
+	},
 }
 
-wezterm.on("update-right-status", function(window, pane)
-	-- "Wed Mar 3 08:14"
-	local date = wezterm.strftime("%a %b %-d %H:%M ")
+resurrect.state_manager.periodic_save({
+	interval_seconds = 300,
+	save_workspaces = true,
+	save_windows = true,
+	save_tabs = true,
+})
+resurrect.state_manager.set_max_nlines(50000)
 
-	local bat = ""
-	for _, b in ipairs(wezterm.battery_info()) do
-		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
-	end
+resurrect.state_manager.set_encryption({
+	enable = true,
+	method = "age",
+	private_key = wezterm.home_dir .. ".config/wezterm/resurrect_secret.txt",
+	public_key = "age1yzgt8vkltmfuf3cfj0ywt6t27clj80c4k5kktg437x0vuxryvp5qnkrq08",
+})
 
-	window:set_right_status(wezterm.format({
-		{ Text = bat .. "   " .. date },
-	}))
+wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
+
+wezterm.on("augment-command-palette", function(window, pane)
+	local workspace_state = resurrect.workspace_state
+	return {
+		{
+			brief = "Window | Workspace: Switch Workspace",
+			icon = "md_briefcase_arrow_up_down",
+			action = workspace_switcher.switch_workspace(),
+		},
+		{
+			brief = "Window | Workspace: Rename Workspace",
+			icon = "md_briefcase_edit",
+			action = wezterm.action.PromptInputLine({
+				description = "Enter new name for workspace",
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+						resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+					end
+				end),
+			}),
+		},
+	}
 end)
 
--- NOTE: consider using resurrect plugin for saving/restoring sessions
--- local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
---
--- resurrect.state_manager.periodic_save({
--- 	interval_seconds = 15,
--- 	save_workspaces = false,
--- 	save_windows = true,
--- 	save_tabs = true,
--- })
---
--- resurrect.state_manager.set_max_nlines(5000)
+toggle_terminal.apply_to_config(config, {
+	key = ";",
+	mods = "CTRL",
+	direction = "Down", -- Direction to split the pane
+	size = { Percent = 20 }, -- Size of the split pane
+	change_invoker_id_everytime = false, -- Change invoker pane on every toggle
+	zoom = {
+		auto_zoom_toggle_terminal = false, -- Automatically zoom toggle terminal pane
+		auto_zoom_invoker_pane = true, -- Automatically zoom invoker pane
+		remember_zoomed = true, -- Automatically re-zoom the toggle pane if it was zoomed before switching away
+	},
+})
 
--- resurrect.state_manager.set_encryption({
--- 	enable = false,
--- 	private_key = wezterm.home_dir .. "/.age/resurrect.txt",
--- 	public_key = "age1ddyj7qftw3z5ty84gyns25m0yc92e2amm3xur3udwh2262pa5afqe3elg7",
--- })
+-- To update the plugins, run
+-- wezterm.plugin.update_all()
 
--- wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
---
--- wezterm.on("resurrect.error", function(err)
--- 	wezterm.log_error("ERROR!")
--- 	wezterm.gui.gui_windows()[1]:toast_notification("resurrect", err, nil, 3000)
--- end)
+-- To check plugins, run
+-- wezterm.plugin.list()
 
 -- and finally, return the configuration to wezterm
 return config
