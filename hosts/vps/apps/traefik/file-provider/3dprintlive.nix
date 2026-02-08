910b73d906e7da62e471c3a7c4041fc25f6dd2f2
Yomi Ikuru <captyomjnr@gmail.com>
2026-02-08 14:50:26 +0000

    fix(vps): traefik config definition clash

--- hosts/vps/apps/traefik/file-provider/3dprintlive.nix
+++ hosts/vps/apps/traefik/file-provider/3dprintlive.nix
@@ -1,17 +1,17 @@
 {
   services.traefik.dynamicConfigOptions = {
-    http.routers.jellyfin = {
+    http.routers."3dprintlive" = {
       rule = ''Host("3dprintlive.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "3dprintlive";
     };
-    http.routers.jellyfin-secure = {
+    http.routers."3dprintlive-secure" = {
       rule = ''Host("3dprintlive.{{env `MEDIA_DOMAIN_NAME`}}")'';
       service = "3dprintlive";
       tls.certResolver = "letsencrypt";
       middlewares = ["global_rate_limit@file"];
     };
 
-    http.services.jellyfin.loadBalancer.servers = [
+    http.services."3dprintlive".loadBalancer.servers = [
       {url = ''https://3dprintlive.local.{{env `MEDIA_DOMAIN_NAME`}}'';}
     ];
   };
