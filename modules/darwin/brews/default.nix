827bf1622d93d4e1d211be301b9fa565a103c180
Yomi Ikuru <captyomjnr@gmail.com>
2026-04-08 00:25:47 +0100

    feat(darwin/brews): add imagemagick brew

--- modules/darwin/brews/default.nix
+++ modules/darwin/brews/default.nix
@@ -5,6 +5,10 @@
   group_azureDev = import ./groups/azure-dev.nix;
 
   docker = import ./docker.nix;
+  imagemagick = import ./imagemagick.nix;
   llama-swap = import ./llama-swap.nix;
   sdl2 = import ./sdl2.nix;
+
+  # Default brews that don't fit into a specific group
+  homebrew.brews = ["mas" "python@3.12"];
 }
