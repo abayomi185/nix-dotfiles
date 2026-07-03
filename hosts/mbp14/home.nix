540fd7a9395fb33f9cf66def73ae5182ba110964
Yomi Ikuru <captyomjnr@gmail.com>
2026-07-03 15:01:49 +0100

    feat(mbp14): enable helix home module

--- hosts/mbp14/home.nix
+++ hosts/mbp14/home.nix
@@ -12,6 +12,7 @@ in {
     # Apps - See ../../modules/home-manager/apps/default.nix
     outputs.homeManagerModules.apps.bat
     outputs.homeManagerModules.apps.discord
+    outputs.homeManagerModules.apps.helix
     outputs.homeManagerModules.apps.jq
     outputs.homeManagerModules.apps.k9s
     outputs.homeManagerModules.apps.karabiner-elements
