61309e72a9570b8054a86c57c93f19c98d229dd2
Yomi Ikuru <captyomjnr@gmail.com>
2026-08-08 18:07:39 +0100

    feat(home/apps): add hass-cli

--- hosts/mstdo/home.nix
+++ hosts/mstdo/home.nix
@@ -3,6 +3,7 @@
     # Apps - See ../../modules/home-manager/apps/default.nix
     outputs.homeManagerModules.apps.bat
     outputs.homeManagerModules.apps.discord
+    outputs.homeManagerModules.apps.hass-cli
     outputs.homeManagerModules.apps.jq
     outputs.homeManagerModules.apps.k9s
     outputs.homeManagerModules.apps.karabiner-elements
